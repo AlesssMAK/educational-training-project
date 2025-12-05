bc20dab5a13f7580e2422b02a37c9586466cf9e1
Iryna-M-html <ivmohnataya@gmail.com>
2025-12-05 09:12:17 +0200

    header is elemeni in console

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,13 +2,12 @@
 import { useTranslations } from "next-intl";
 import css from "./Header.module.css";
 import Link from "next/link";
-const Hero = () => {
-  const t = useTranslations("StyleSection");
+export default function Header() {
   return (
     <header className={css.section}>
       <div className="container">
         <div className={css.header}>
-          <Link href="/" aria-label={t("home")} className={css.logoLink}>
+          <Link href="/" className={css.logoLink}>
             {/* <svg width="84" height="36" aria-hidden="true">
               <use href="/symbol-defs.svg#icon-company-logo"></use>
             </svg> */}
@@ -17,4 +16,4 @@ const Hero = () => {
       </div>
     </header>
   );
-};
+}
